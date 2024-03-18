4823e212308333dd7a3aec10cb3d9aeed6928500
GustavoDMS <pipe.gus2014@gmail.com>
2024-03-18 09:04:53 -0300

    jogo Genius

---
+++ GENIUS.py
@@ -0,0 +1,33 @@
+import random
+
+sorteados = []
+flagCerto = True
+entrada = ""
+while flagCerto:
+    sorteados.append(random.randint(0, 3))
+
+    for i in sorteados:
+        if(i == 0):
+            print("led_amarelo")
+        elif(i == 1):
+            print("led_verde")
+        elif(i == 2):
+            print("led_vermelho")
+        elif(i == 3):
+            print("led_azul")
+
+    for i in sorteados:
+        entrada = ""
+        # while len(entrada) < 0:
+        entrada = int(input("Digite a sequência: "))
+        if(entrada == i):
+            pass
+        else:
+            print("Errou")
+            flagCerto = False
+            break
+    print("Parabéns")
+
+
+
+
